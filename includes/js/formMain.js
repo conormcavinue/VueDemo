0b14ca181029ef97f5d02fc1f11fb5b490743b88
Conor <conor.mcavinue@kpmg.ie>
2018-05-30 11:18:33 +0100

    Removed the videos section

--- includes/js/formMain.js
+++ includes/js/formMain.js
@@ -8,25 +8,10 @@ Vue.component('person', {
     template: "<div class='row'><div class='col s3'>{{ firstName }} {{ lastName }}</div><div class='col s3'>{{ email }}</div><div class='col s6'>{{ address }}</div></div>"
 });
 
-Vue.component('video-list', {
-    props: {
-        src: String,
-
-    },
-    template: "<video class='video-js' controls preload='auto' data-setup='{}'><source src='{{ src }}' type='video/mp4'></video>"
-});
-
 var vm = new Vue({
     el: '#app',
     data: {
         people: [],
-        videos: [
-             "http://localhost/videos/RU.2016.EOYT.Week.1.Ireland.v.New.Zealand.SD.NZ.Commentary.1st.Half.x264.mp4",
-             "http://localhost/videos/RU.2016.EOYT.Week.1.Ireland.v.New.Zealand.SD.NZ.Commentary.2nd.Half.x264.mp4",
-             "http://localhost/videos/Preacher.S02E01.mp4",
-             "http://localhost/videos/boxed-join.mp4"
-        ],
-
         firstName: '',
         lastName: '',
         email: '',
